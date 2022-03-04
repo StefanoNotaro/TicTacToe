1b391e53f768b7617fd46f2461f9d99815d61a6c
Stefano Notaro Francesco <stefano.notaro@gmail.com>
2022-03-04 17:02:27 -0300

    style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TicTacToe';
+  private renderer: Renderer2;
+
+  constructor(public rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  ngOnInit(): void {
+    this.renderer.addClass(document.body, 'custom-body');
+  }
 }
